271103e1ad61043c11c1cb9603b25054b359e816
kmoon <1218moonlight@gmail.com>
2019-05-10 15:39:58 +0900

    수치 결정 버튼 추가. 数値決定button追加。

--- src/gui/ui.go
+++ src/gui/ui.go
@@ -2,20 +2,24 @@ package gui
 
 import (
 	"github.com/andlabs/ui"
+	"fmt"
 )
 
 type mainBox struct {
 	vertical *ui.Box
 	barBox   barBox
+	button   okBtn
 }
 
 func newMainBox() mainBox {
-	box := mainBox{vertical: func() *ui.Box {
-		box := ui.NewVerticalBox()
-		box.SetPadded(true)
-		return box
-	}(),
-		barBox: newBarBox()}
+	box := mainBox{
+		vertical: func() *ui.Box {
+			box := ui.NewVerticalBox()
+			box.SetPadded(true)
+			return box
+		}(),
+		barBox: newBarBox(),
+		button: newOkBtn()}
 
 	return box
 }
@@ -26,6 +30,21 @@ func (m mainBox) settingsBar() {
 
 	m.barBox.vertical.Append(spinbox, false)
 	m.barBox.vertical.Append(slider, false)
+	m.barBox.vertical.Append(m.button.btn, false)
+
+	spinbox.OnChanged(func(spinbox *ui.Spinbox) {
+		slider.SetValue(spinbox.Value())
+	})
+
+	slider.OnChanged(func(slider *ui.Slider) {
+		spinbox.SetValue(slider.Value())
+	})
+
+	m.button.btn.OnClicked(func(button *ui.Button) {
+		m.button.number = slider.Value()
+		fmt.Println("[] : ", m.button.number)
+		// Todo
+	})
 
 	m.barBox.group.SetChild(m.barBox.vertical)
 
@@ -51,4 +70,18 @@ func newBarBox() barBox {
 		}()}
 
 	return b
+}
+
+type okBtn struct {
+	number int
+	btn *ui.Button
+}
+
+func newOkBtn() okBtn {
+	o := okBtn{
+		btn: func() *ui.Button {
+			return ui.NewButton("Okay")
+		}()}
+
+	return o
 }
